e2acfbf5ec98dd73157deb6c5dad34373bf7f178
Zach Nusbaum <nusbaumz@msu.edu>
2016-01-21 10:22:03 -0700

    updated

--- convert.rb
+++ convert.rb
@@ -2,9 +2,31 @@ require 'xmlsimple'
 config = XmlSimple.xml_in('Schwienhorst 09-12-13.xml', {'KeyAttr' => 'name'})
 base = config['ALL_INSTANCES'][0]['instance']
 
-base.each do |instance|
-	p instance
+i = 1
+
+base.each do |x|
+	if x['code'][0].start_with?('Clicker') || x['code'][0].start_with?('Task')
+		puts "Instance#: #{x['ID'][0]}"
+		puts "Code: #{x['code'][0]}"
+		# puts "#{x['label']}"
+
+		tags = x['label']
+
+		tags_array = []
+
+		tags.each do |tag|
+			tags_array << tag['text'][0]
+		end
+
+		p tags_array
+		puts "\n"
+
+	else
+		next
+	end
 end
+
+
 
 #xml = File.read("Schwienhorst 09-12-13.xml")
 #object_instance = OpenStruct.new( Hash.from_xml(xml) )
